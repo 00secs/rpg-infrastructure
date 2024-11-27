0a1a7bd5d3a78caab941bee5d9411f6ab8c6ee98
Tengu712 <genreihoutengu@gmail.com>
2024-11-27 21:56:24 +0900

    feat: ウィンドウを作成

---
+++ src/engine.rs
@@ -0,0 +1,88 @@
+use crate::EError;
+use std::sync::Arc;
+use winit::{application::*, dpi::*, event::*, event_loop::*, window::*};
+
+/// アプリケーションの基本情報。
+pub struct ApplicationInfo {
+    pub title: &'static str,
+    pub width: f32,
+    pub height: f32,
+    pub is_fullscreen: bool,
+}
+
+/// winitベースウィンドウアプリケーションの構造体。
+struct Application {
+    info: ApplicationInfo,
+    window: Option<Arc<Window>>,
+}
+
+impl ApplicationHandler for Application {
+    /// アプリケーションが再開されたときに呼ばれるメソッド。
+    ///
+    /// ことWindows, macOS, Linuxにおいてはアプリケーション起動直後に一度だけ呼ばれる。
+    /// そのため、アプリケーションに必要なすべてのオブジェクトを初期化する。
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        if self.window.is_some() {
+            return;
+        }
+
+        let primary_monitor = event_loop
+            .primary_monitor()
+            .expect("no primary monitor is found.");
+
+        let fullscreen = if self.info.is_fullscreen {
+            Some(Fullscreen::Borderless(Some(primary_monitor)))
+        } else {
+            None
+        };
+
+        let window_attributes = Window::default_attributes()
+            .with_title(self.info.title)
+            .with_resizable(false)
+            .with_inner_size(LogicalSize::new(self.info.width, self.info.height))
+            .with_fullscreen(fullscreen);
+        let window = event_loop
+            .create_window(window_attributes)
+            .expect("failed to create a window.");
+        window.set_enabled_buttons(WindowButtons::CLOSE | WindowButtons::MINIMIZE);
+
+        let window = Arc::new(window);
+
+        self.window = Some(window);
+    }
+
+    /// ウィンドウイベントを処理するメソッド。
+    ///
+    /// - ウィンドウ破棄イベント -> アプリケーション終了
+    /// - キーボード入力イベント -> InputManager
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, _: WindowId, event: WindowEvent) {
+        if self.window.is_none() {
+            return;
+        }
+
+        match event {
+            WindowEvent::CloseRequested => event_loop.exit(),
+            WindowEvent::Destroyed => event_loop.exit(),
+            _ => (),
+        }
+    }
+
+    /// デッドタイムに呼ばれるメソッド。
+    ///
+    /// つまり、アプリケーションのメインループ。
+    fn about_to_wait(&mut self, _: &ActiveEventLoop) {
+        if self.window.is_none() {
+            return;
+        }
+    }
+}
+
+/// winitベースウィンドウアプリケーションを実行する関数。
+///
+/// ウィンドウが閉じられるまでスレッドを待機する。
+pub fn run(info: ApplicationInfo) -> Result<(), EError> {
+    let event_loop = EventLoop::new()?;
+    event_loop.set_control_flow(ControlFlow::Poll);
+    event_loop.run_app(&mut Application { info, window: None })?;
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+mod engine;
+
+type EError = Box<dyn std::error::Error>;
+
 fn main() {
-    println!("Hello, world!");
+    const SCENE_WIDTH: u32 = 1280;
+    const SCENE_HEIGHT: u32 = 720;
+
+    engine::run(engine::ApplicationInfo {
+        title: "タイトル",
+        width: SCENE_WIDTH as f32,
+        height: SCENE_HEIGHT as f32,
+        is_fullscreen: false,
+    })
+    .unwrap();
 }
